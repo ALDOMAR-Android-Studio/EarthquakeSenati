94c204dc78a29b25ce8742f10eec7cce37780de4
Aldo Omar Morales Carlos <aomc06@gmail.com>
2025-04-09 20:25:27 -0500

    Agregar elementos de RecyclerView

--- app/src/main/java/com/aldomar/earthquakesenati/MonitorActivity.java
+++ app/src/main/java/com/aldomar/earthquakesenati/MonitorActivity.java
@@ -1,5 +1,6 @@
 package com.aldomar.earthquakesenati;
 
+import android.app.Instrumentation;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.aldomar.earthquakesenati.databinding.ActivityMonitorBinding;
+
 public class MonitorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_monitor);
+
+        ActivityMonitorBinding binding = ActivityMonitorBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
