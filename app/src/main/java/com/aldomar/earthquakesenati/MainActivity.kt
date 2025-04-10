cbf07a7899c5fed889f4a6f7eccf2ee26244c3d5
Aldo Omar Morales Carlos <aomc06@gmail.com>
2025-04-09 19:24:44 -0500

    Archivos nuevo proyecto

---
+++ app/src/main/java/com/aldomar/earthquakesenati/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.aldomar.earthquakesenati
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aldomar.earthquakesenati.ui.theme.EarthquakeSenatiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EarthquakeSenatiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EarthquakeSenatiTheme {
+        Greeting("Android")
+    }
+}
